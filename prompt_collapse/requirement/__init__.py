8466eb6289c3cfe6f635c44a8987899b4c446ce0
kraglik <kraglik.i.d@gmail.com>
2024-12-15 12:20:18 +0400

    update

--- prompt_collapse/requirement/__init__.py
+++ prompt_collapse/requirement/__init__.py
@@ -7,6 +7,7 @@ from .none_of import NoneOfRequirement
 from .registry import REQUIREMENT_REGISTRY
 from .requirement import Requirement
 from .some_of import SomeOfRequirement
+from .value_in import OneOfRequirement
 
 REQUIREMENT_REGISTRY.register("less_than", LessThanRequirement)
 REQUIREMENT_REGISTRY.register("greater_than", GreaterThanRequirement)
@@ -15,12 +16,14 @@ REQUIREMENT_REGISTRY.register("all_of", AllOfRequirement)
 REQUIREMENT_REGISTRY.register("some_of", SomeOfRequirement)
 REQUIREMENT_REGISTRY.register("none_of", NoneOfRequirement)
 REQUIREMENT_REGISTRY.register("equals", EqualsRequirement)
+REQUIREMENT_REGISTRY.register("one_of", OneOfRequirement)
 
 REQUIREMENT_REGISTRY.register("lt", LessThanRequirement)
 REQUIREMENT_REGISTRY.register("gt", GreaterThanRequirement)
 REQUIREMENT_REGISTRY.register("and", AllOfRequirement)
 REQUIREMENT_REGISTRY.register("or", SomeOfRequirement)
 REQUIREMENT_REGISTRY.register("not", NoneOfRequirement)
+REQUIREMENT_REGISTRY.register("in", OneOfRequirement)
 
 __all__ = [
     "Requirement",
@@ -31,5 +34,6 @@ __all__ = [
     "SomeOfRequirement",
     "NoneOfRequirement",
     "EqualsRequirement",
+    "OneOfRequirement",
     "REQUIREMENT_REGISTRY",
 ]
